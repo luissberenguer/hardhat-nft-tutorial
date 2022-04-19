fa4e05b11ee077969f7c1f5670c65f9cbef32de6
Luis Berenguer <luis01developer@gmail.com>
2022-04-19 16:45:31 +0200

    add deploy script and constants

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xE5FcF1DB1F90339DBe51e4E13ebC5527F4C62055";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL =
+  "https://crypto-devs-landing-5zrup2i9h-luissbereguer.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
